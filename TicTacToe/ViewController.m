1e47b461bc43208aa23d9d09d91b798c6e6ce2f1
Mark Porcella <markporcella67@gmail.com>
2015-05-14 14:39:22 -0700

    labels added and IBOutlets for the labels added

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -9,8 +9,23 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *labelOne;
+@property (weak, nonatomic) IBOutlet UILabel *labelTwo;
+@property (weak, nonatomic) IBOutlet UILabel *labelThree;
+@property (weak, nonatomic) IBOutlet UILabel *labelFour;
+@property (weak, nonatomic) IBOutlet UILabel *labelFive;
+@property (weak, nonatomic) IBOutlet UILabel *labelSix;
+@property (weak, nonatomic) IBOutlet UILabel *labelSeven;
+@property (weak, nonatomic) IBOutlet UILabel *labelEight;
+@property (weak, nonatomic) IBOutlet UILabel *labelNine;
+@property (weak, nonatomic) IBOutlet UILabel *whichPlayerLabel;
 
 @end
+
+
+
+
+
 
 @implementation ViewController
 
@@ -19,6 +34,15 @@
 
 }
 
+- (void) findLabelUsingPoint:(CGPoint)point
+{
+
+}
+
+- (IBAction)onLabelTapped:(UITapGestureRecognizer *)sender {
+    
+
+}
 
 
 @end
