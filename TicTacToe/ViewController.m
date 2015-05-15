8b95f0a40913b09789794c7e74d73004bdeaad3e
Mark Porcella <markporcella67@gmail.com>
2015-05-14 18:29:58 -0700

    Adding to a Xplayer and Yplayer array, every label that is clicked

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -20,27 +20,26 @@
 @property (weak, nonatomic) IBOutlet UILabel *labelNine;
 @property (weak, nonatomic) IBOutlet UILabel *whichPlayerLabel;
 @property BOOL yourTurn;
-
+@property NSMutableArray *xArray;
+@property NSMutableArray *yArray;
 @end
-
-
-
-
-
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.yourTurn = YES;
-
+    self.xArray = [NSMutableArray new];
+    self.yArray = [NSMutableArray new];
+        //[self.xArray addObject:[NSNumber numberWithInt:self.labelNine.tag]];
+    NSLog(@"%@", self.xArray);
+        //int *ra [8][3]= {{1,2,3},{4,5,6},{7,8,9}, {1,4,7}, {2,5,8}, {3, 6, 9}, {1, 5, 9}, {7, 5, 3}};
 }
 
 - (void) findLabelUsingPoint:(CGPoint)point
 {
 
 }
-
 - (IBAction)onLabelTapped:(UITapGestureRecognizer *)sender {
     CGPoint point = [sender locationInView:self.view];
     NSArray *arrayOfLabels = [NSArray arrayWithObjects:self.labelOne, self.labelTwo, self.labelThree, self.labelFour, self.labelFive, self.labelSix, self.labelSeven, self.labelEight, self.labelNine, nil];
@@ -48,14 +47,27 @@
     for (UILabel *label in arrayOfLabels) {
         if (CGRectContainsPoint(label.frame, point)) {
             label.text = @"X";
+
             if (self.yourTurn) {
                 label.text = @"X";
+                label.backgroundColor = [UIColor blueColor];
+                self.whichPlayerLabel.text = @"O";
                 self.yourTurn = NO;
+                [self.xArray addObject:[NSNumber numberWithInt:label.tag]];
+                NSLog(@"X array %@",self.xArray);
             } else {
                 label.text = @"O";
+                label.backgroundColor = [UIColor redColor];
+                self.whichPlayerLabel.text = @"X";
                 self.yourTurn = YES;
+                [self.yArray addObject:[NSNumber numberWithInt:label.tag]];
+                NSLog(@"Y array %@",self.yArray);
             }
         }
+        //NSArray *arraysOfWinningCombinations = @[@[1, 2, 3], @[4, 5, 6], @[7, 8, 9], @[1, 4, 7], @[2, 5, 8], @[3, 6, 9], @[1, 5, 9] , @[7, 5, 3]];
+        //NSArray *arraysOfWinningCombinations = [NSArray arrayWithArray:@[[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9] , [7, 5, 3]]];
+        //int *ra [8][3]= {{1,2,3},{4,5,6},{7,8,9}, {1,4,7}, {2,5,8}, {3, 6, 9}, {1, 5, 9}, {7, 5, 3}};
+
 
     }
 }
