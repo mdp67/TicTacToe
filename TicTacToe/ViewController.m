0e7aaa5ee88455649bb7316d0714eeefa6628603
Mark Porcella <markporcella67@gmail.com>
2015-05-15 19:08:39 -0700

    logic to move the position label, changing labels based upon where user drags them, getting to the logic to have computer play

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -21,10 +21,13 @@
 @property (weak, nonatomic) IBOutlet UILabel *labelNine;
 @property (weak, nonatomic) IBOutlet UILabel *whichPlayerLabel;
 @property BOOL yourTurn;
-@property NSMutableArray *xArray;
-@property NSMutableArray *yArray;
+@property NSMutableArray *userArray;
+@property NSMutableArray *computerArray;
 @property NSString *whoWon;
 @property NSArray *arrayOfLabels;
+@property CGPoint whichPlayerLabelInitialPoint;
+@property NSMutableArray *positionsAvailable;
+@property NSNumber *numberOfPositionsRemaining;
 
 @end
 
@@ -33,11 +36,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.yourTurn = YES;
-    self.xArray = [NSMutableArray new];
-    self.yArray = [NSMutableArray new];
+    self.userArray = [NSMutableArray new];
+    self.computerArray = [NSMutableArray new];
     self.arrayOfLabels = @[self.labelOne, self.labelTwo, self.labelThree, self.labelFour, self.labelFive, self.labelSix, self.labelSeven, self.labelEight, self.labelNine];
     self.whichPlayerLabel.text = @"X";
     self.whichPlayerLabel.backgroundColor = [UIColor blueColor];
+    self.whichPlayerLabelInitialPoint = self.whichPlayerLabel.center;
+    self.positionsAvailable = [[NSMutableArray alloc] initWithObjects:@1, @2, @3, @4, @5, @6, @7, @8, @9, nil];
+    self.numberOfPositionsRemaining = [[NSNumber alloc] initWithFloat:self.positionsAvailable.count] ;
 }
 
 -(BOOL) checkIfThreeNumberMatchWinnersInArray {
@@ -49,11 +55,10 @@
         int matchingNumberCheckInt = 0;
         NSArray *checkArray = [NSArray new];
         if (self.yourTurn) {
-            checkArray = self.yArray;
+            checkArray = self.computerArray;
         } else {
-            checkArray = self.xArray;
+            checkArray = self.userArray;
         }
-        NSLog(@" check array %@", checkArray);
         for (NSNumber *numberInPlayerArray in checkArray) {
             if ([arrayThatContainsWinners containsObject:numberInPlayerArray]) {
                 matchingNumberCheckInt++;
@@ -73,58 +78,85 @@
 }
 - (IBAction)onXODrag:(UIPanGestureRecognizer *)sender {
     CGPoint dragPoint = [sender locationInView:self.view];
-
     if (CGRectContainsPoint(self.whichPlayerLabel.frame, dragPoint)) {
-        NSLog(@"Dragging into the point");
+        self.whichPlayerLabel.center = dragPoint;
     }
 
+    if (sender.state == UIGestureRecognizerStateEnded) {
+        CGPoint releasePoint = [sender locationInView:self.view];
+        for (UILabel *label in self.arrayOfLabels) {
+            if (CGRectContainsPoint(label.frame, releasePoint)) {
+                [self play:label thePoint:releasePoint];
+                self.whichPlayerLabel.center = self.whichPlayerLabelInitialPoint;
+                return;
+            }
+        }
+        [UIView animateWithDuration:0.5 animations:^{
+            self.whichPlayerLabel.center = self.whichPlayerLabelInitialPoint;
+        }];
+    }
 }
 
 - (IBAction)onLabelTapped:(UITapGestureRecognizer *)sender {
     CGPoint point = [sender locationInView:self.view];
-
     for (UILabel *label in self.arrayOfLabels) {
-        if (CGRectContainsPoint(label.frame, point) && label.enabled) {
+        [self play:label thePoint:point];
+    }
+}
+
+- (void) play:(UILabel *)label thePoint:(CGPoint) point {
+    if (CGRectContainsPoint(label.frame, point) && label.enabled) {
+        label.text = @"X";
+        if (self.yourTurn) {
             label.text = @"X";
-            if (self.yourTurn) {
-                label.text = @"X";
-                label.backgroundColor = [UIColor blueColor];
-                self.whichPlayerLabel.text = @"O";
-                self.yourTurn = NO;
-                [self.xArray addObject:[NSNumber numberWithInt:label.tag]];
-                //NSLog(@"X array %@",self.xArray);
-                NSLog(@"got here");
-            } else {
-                label.text = @"O";
-                label.backgroundColor = [UIColor redColor];
-                self.whichPlayerLabel.text = @"X";
-                self.yourTurn = YES;
-                [self.yArray addObject:[NSNumber numberWithInt:label.tag]];
-                //NSLog(@"Y array %@",self.yArray);
-            }
-            label.enabled = NO;
+            label.backgroundColor = [UIColor blueColor];
+            self.whichPlayerLabel.text = @"O";
+            self.yourTurn = NO;
+            self.whichPlayerLabel.backgroundColor = [UIColor redColor];
+            NSNumber *addNumber = [[NSNumber alloc] initWithFloat:label.tag];
+            [self.userArray addObject:addNumber];
+        } else {
+            label.text = @"O";
+            label.backgroundColor = [UIColor redColor];
+            self.whichPlayerLabel.text = @"X";
+            self.yourTurn = YES;
+            self.whichPlayerLabel.backgroundColor = [UIColor blueColor];
+            NSNumber *addNumber = [[NSNumber alloc] initWithFloat:label.tag];
+            [self.computerArray addObject:addNumber];
         }
+        label.enabled = NO;
+        NSNumber *removeNumber = [[NSNumber alloc] initWithFloat:label.tag];
+        [self.positionsAvailable removeObject:removeNumber];
+        NSLog(@"%@", self.positionsAvailable);
+
     }
     if ([self checkIfThreeNumberMatchWinnersInArray]) {
         NSString *alertViewTitle = [self.whoWon stringByAppendingString:@" won the Game!"];
         UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:alertViewTitle message:@"It was a great game" delegate:nil cancelButtonTitle:@"Play again?" otherButtonTitles:nil, nil];
         [alertView show];
-        [self resetAllLabels];
+        [self resetGame];
+    }
+    [self computerChoosePoint];
+
+}
+
+- (void) computerChoosePoint {
+    if (self.yourTurn == NO) {
+        NSLog(@"computer's turn");
     }
 }
 
-- (void) play:(UILabel *)label thePoint:(CGPoint) point {
-
-
-
-}
-
-- (void) resetAllLabels {
+- (void) resetGame {
     for (UILabel *label in self.arrayOfLabels) {
         label.text = @"";
         label.backgroundColor = [UIColor lightGrayColor];
+        label.enabled = TRUE;
     }
+    self.whichPlayerLabel.backgroundColor = [UIColor blueColor];
     self.whichPlayerLabel.text = @"X";
+    [self.userArray removeAllObjects];
+    [self.computerArray removeAllObjects];
+    self.yourTurn = YES;
 }
 
 
